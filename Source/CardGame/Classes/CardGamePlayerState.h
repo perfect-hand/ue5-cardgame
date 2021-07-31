30159cf4c93559a1b59ffebfebe2f5d9e9e5c5b9
Nick Pruehs <dev@npruehs.de>
2021-07-31 20:22:43 +0200

    Replicate player index.

--- Source/CardGame/Classes/CardGamePlayerState.h
+++ Source/CardGame/Classes/CardGamePlayerState.h
@@ -11,9 +11,13 @@ class CARDGAME_API ACardGamePlayerState : public APlayerState
 	GENERATED_BODY()
 
 public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void CopyProperties(APlayerState* PlayerState) override;
+	
 	int32 GetPlayerIndex() const;
 	void SetPlayerIndex(int32 InPlayerIndex);
 	
 private:
+	UPROPERTY(Replicated)
 	int32 PlayerIndex;
 };
--- Source/CardGame/Private/CardGamePlayerState.cpp
+++ Source/CardGame/Private/CardGamePlayerState.cpp
@@ -1,5 +1,28 @@
 ﻿#include "CardGamePlayerState.h"
 
+#include "Net/UnrealNetwork.h"
+
+void ACardGamePlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ACardGamePlayerState, PlayerIndex);
+}
+
+void ACardGamePlayerState::CopyProperties(APlayerState* PlayerState)
+{
+	Super::CopyProperties(PlayerState);
+
+	ACardGamePlayerState* CardGamePlayerState = Cast<ACardGamePlayerState>(PlayerState);
+
+	if (!IsValid(CardGamePlayerState))
+	{
+		return;
+	}
+
+	CardGamePlayerState->SetPlayerIndex(PlayerIndex);
+}
+
 int32 ACardGamePlayerState::GetPlayerIndex() const
 {
 	return PlayerIndex;
