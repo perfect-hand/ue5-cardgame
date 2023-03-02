698216a094dc2b3c2e950ec4b09aa867567a007b
Nick Pruehs <dev@npruehs.de>
2023-03-03 00:20:25 +0100

    Set card actor instance id.

--- Source/CardGame/Private/CardGameActor.cpp
+++ Source/CardGame/Private/CardGameActor.cpp
@@ -3,11 +3,18 @@
 void ACardGameActor::Init(const FCardGameCardModel& Model, UCardGameCardPile* CardPileClass, int32 PositionInCardPile,
 	TOptional<uint8> PlayerIndex)
 {
+	InstanceId = Model.InstanceId;
+	
 	NotifyOnInit(Model, CardPileClass, PositionInCardPile, PlayerIndex);
 }
 
+int64 ACardGameActor::GetInstanceId() const
+{
+	return InstanceId;
+}
+
 void ACardGameActor::NotifyOnInit(const FCardGameCardModel& Model, UCardGameCardPile* CardPileClass,
-	int32 PositionInCardPile, TOptional<uint8> PlayerIndex)
+                                  int32 PositionInCardPile, TOptional<uint8> PlayerIndex)
 {
 	ReceiveOnInit(Model, CardPileClass, PositionInCardPile, PlayerIndex.Get(0), PlayerIndex.IsSet());
 }
